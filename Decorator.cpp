d30a6c242df72acd7330f361992e2a9eda7c953c
peraza97 <omarperaza19@gmail.com>
2016-10-24 14:28:28 -0700

    new

--- Decorator.cpp
+++ Decorator.cpp
@@ -1,1 +1,11 @@
 #include "Decorator.h"
+
+Decorator:: Decorator()
+{
+  child = 0;
+}
+
+Decorator:: Decorator(Base * c)
+{
+  child = c;
+}
--- Decorator.h
+++ Decorator.h
@@ -1,11 +1,13 @@
 #ifndef DECORATOR_H
 #define DECORATOR_H
-
+#include "Base.h"
 class Decorator: public Base
 {
   protected:
   Base * child;
   public:
+  Decorator();
+  Decorator(Base *);
   virtual double evaluate() = 0;
 };
 
